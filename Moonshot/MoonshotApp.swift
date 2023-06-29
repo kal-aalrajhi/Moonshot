459a80e1734dfa8e210cca8e347591798fc36c57
Kal <kal.aalrajhi@gmail.com>
2023-06-29 10:18:08 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Dr Cpt Blackbeard on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
